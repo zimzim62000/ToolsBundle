539944335aa791ac2c7e22ea4e59a066d0fa9067
zimzim62000 <zimzim62000@gmail.com>
2015-06-17 17:44:46 +0200

    add security context

--- Model/APYDataGrid/ApyDataGridRepositoryInterface.php
+++ Model/APYDataGrid/ApyDataGridRepositoryInterface.php
@@ -4,10 +4,11 @@
 namespace ZIMZIM\ToolsBundle\Model\APYDataGrid;
 
 use APY\DataGridBundle\Grid\Source\Entity;
+use Symfony\Component\Security\Core\SecurityContext;
 
 interface ApyDataGridRepositoryInterface{
 
     /** @return source */
-    public function getList(Entity $source);
+    public function getList(Entity $source, SecurityContext $securityContext);
 
 }
