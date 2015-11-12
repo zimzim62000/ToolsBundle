ac29ce0cc71994a55a5956da05df670ce364bbda
zimzim62000 <zimzim62000@gmail.com>
2015-11-12 14:01:57 +0100

    fix

--- Model/APYDataGrid/ApyDataGridRepositoryInterface.php
+++ Model/APYDataGrid/ApyDataGridRepositoryInterface.php
@@ -10,6 +10,6 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 interface ApyDataGridRepositoryInterface{
 
     /** @return source */
-    public function getList(Entity $source, TokenStorage $token, SecurityChecker $securityChecker )
+    public function getList(Entity $source, TokenStorage $token, SecurityChecker $securityChecker );
 
 }
