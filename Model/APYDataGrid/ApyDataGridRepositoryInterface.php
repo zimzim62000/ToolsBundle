a19cf5ef487597c104569245b80a68107f4cf2b4
zimzim62000 <zimzim62000@gmail.com>
2015-11-12 14:43:20 +0100

    fix

--- Model/APYDataGrid/ApyDataGridRepositoryInterface.php
+++ Model/APYDataGrid/ApyDataGridRepositoryInterface.php
@@ -4,10 +4,10 @@
 namespace ZIMZIM\ToolsBundle\Model\APYDataGrid;
 
 use APY\DataGridBundle\Grid\Source\Entity;
-use SensioLabs\Security\SecurityChecker;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
 interface ApyDataGridRepositoryInterface{
     /** @return source */
-    public function getList(Entity $source, TokenStorage $token, SecurityChecker $securityChecker );
+    public function getList(Entity $source, TokenStorage $token, AuthorizationChecker $checker );
 }
