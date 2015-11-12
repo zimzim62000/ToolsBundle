b6457e1daec2011049805f760b4142d4c422722d
zimzim62000 <zimzim62000@gmail.com>
2015-11-12 09:04:19 +0100

    add seo file

---
+++ Model/MetaSEO.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace ZIMZIM\ToolsBundle\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+use APY\DataGridBundle\Grid\Mapping as GRID;
+
+trait MetaSEO{
+
+    /**
+     * @var string
+     *
+     * @GRID\Column(operatorsVisible=false, visible=false, filterable=false)
+     * @ORM\Column(name="title_seo", type="string", length=255)
+     */
+    public $titleSeo;
+
+    /**
+     * @var string
+     *
+     * @GRID\Column(operatorsVisible=false, visible=false, filterable=false)
+     * @ORM\Column(name="desc_seo, type="text")
+     */
+    public $descSeo;
+
+    /**
+     * @return string
+     */
+    public function getTitleSeo()
+    {
+        return $this->titleSeo;
+    }
+
+    /**
+     * @param string $titleSeo
+     *
+     * @return Object use this trait
+     */
+    public function setTitleSeo($titleSeo)
+    {
+        $this->titleSeo = $titleSeo;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescSeo()
+    {
+        return $this->descSeo;
+    }
+
+    /**
+     * @param string $descSeo
+     *
+     * @return Object use this trait
+     */
+    public function setDescSeo($descSeo)
+    {
+        $this->descSeo = $descSeo;
+
+        return $this;
+    }
+}
