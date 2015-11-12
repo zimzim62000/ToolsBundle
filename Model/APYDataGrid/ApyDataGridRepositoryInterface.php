ccdc5422e46523280b05958fc90a617bfb19d9da
zimzim62000 <zimzim62000@gmail.com>
2015-11-12 11:27:00 +0100

    update for symfony 3.0

--- Model/APYDataGrid/ApyDataGridRepositoryInterface.php
+++ Model/APYDataGrid/ApyDataGridRepositoryInterface.php
@@ -4,11 +4,12 @@
 namespace ZIMZIM\ToolsBundle\Model\APYDataGrid;
 
 use APY\DataGridBundle\Grid\Source\Entity;
-use Symfony\Component\Security\Core\SecurityContext;
+use SensioLabs\Security\SecurityChecker;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 interface ApyDataGridRepositoryInterface{
 
     /** @return source */
-    public function getList(Entity $source, SecurityContext $securityContext);
+    public function getList(Entity $source, TokenStorage $token, SecurityChecker $securityChecker )
 
 }
